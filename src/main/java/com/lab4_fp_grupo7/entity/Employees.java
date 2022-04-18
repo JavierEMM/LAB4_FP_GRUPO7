5951a6c5661d8dfed592cf39ff54e5ff8f2f2fc7
Javier Marroquin <a20193644@pucp.edu.pe>
2022-04-18 10:39:50 -0500

    Archivos actualizados

---
+++ src/main/java/com/lab4_fp_grupo7/entity/Employees.java
@@ -0,0 +1,11 @@
+package com.lab4_fp_grupo7.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="employees")
+public class Employees {
+
+    //COMPLETAR
+}
---
+++ src/main/java/com/lab4_fp_grupo7/repository/HistoryRepository.java
@@ -0,0 +1,14 @@
+package com.lab4_fp_grupo7.repository;
+
+import com.example.laboratorio4.entity.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History,Integer> {
+
+    //COMPLETAR
+
+}
