e8f6728b5711bdf7764f2c61bd4fe33cee29e964
Javier Marroquin <a20193644@pucp.edu.pe>
2022-04-18 10:37:59 -0500

    Hola

---
+++ src/main/java/com/lab4_fp_grupo7/Lab4FpGrupo7Application.java
@@ -0,0 +1,13 @@
+package com.lab4_fp_grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4FpGrupo7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4FpGrupo7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab4_fp_grupo7/Lab4FpGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab4_fp_grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4FpGrupo7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
